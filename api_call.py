c0179eda077693ae32d468500e2b169fc6a53f1e
nirpeleg <39925695+nirpeleg@users.noreply.github.com>
2019-12-31 17:21:22 +0200

    Add files via upload

---
+++ api_call.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "http://127.0.0.1:8000/languages"
+
+payload = {}
+headers= {}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+data = json.loads(response.text.encode('utf8'))
+final_dic = {}
+final_dic['languages'] = data
+with open('api_result.json', "w") as file:
+    json.dump(final_dic, file, indent=2)
+
